0eec3ff2544fd44fbefbc7f7e3e5e3a35b46b823
quyenttn-0473 <tran.thi.ngoc.quyen@sun-asterisk.com>
2024-07-09 14:43:49 +0700

    first commit

---
+++ src/config/viewEngine.js
@@ -0,0 +1,7 @@
+// import express from 'express';
+
+let configViewEngine = (app) => {
+    app.set('view engine', 'pug');
+};
+
+module.exports = configViewEngine;
---
+++ src/seeders/20240709055651-demo-user.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        return queryInterface.bulkInsert('Users', [
+            {
+                firstName: 'John',
+                lastName: 'Doe',
+                email: 'example@example.com',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                firstName: 'Ngoc',
+                lastName: 'Quyen',
+                email: 'ngocquyen@gmail.com',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                firstName: 'Mai',
+                lastName: 'Hong',
+                email: 'ngoc@gmail.com',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        ]);
+    },
+
+    async down(queryInterface, Sequelize) {
+        return queryInterface.bulkDelete('Users', null, {});
+    },
+};
